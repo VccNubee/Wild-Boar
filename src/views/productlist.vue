6614d97062f795acd14d516f372a3543528f67af
BigPeiQi <3223364205@qq.com>
2019-02-15 20:57:07 +0800

    临走前更新下

--- src/views/productlist.vue
+++ src/views/productlist.vue
@@ -1,52 +1,35 @@
 <template>
-  <!-- <div class="productlist">
-  	<div  class="topMenuBar">
-  		<div  class="topbar" id="topBar" style="background: rgb(255, 255, 255);">
-  			<div class="left">
-  				<a class="back">
-  					<span class="iconfont mei-arrow">
-  						
-  					</span>
-  				</a>
-  			</div> 
-  			<div class="title">
-  				<span >服饰配饰专场</span> 
-  			</div> 
-  			<div class="right">
-  				<div class="switch-menu">
-  					<a class="show-menu">
-  						<span class="iconfont mei-dot">
-  							
-  						</span>
-  					</a> 
-  				</div> 
-  				</div>
-  			</div> 
-  			<div class="menu">
-  				<div  class="box">
-  					<ul>
-  						<li class="current">人气</li> 
-  						<li class="">折扣</li> 
-  						<li class="price up">
-  							<span>价格</span>
-  						</li> 
-  						<li class="filter">筛选</li>
-  					</ul>
-  				</div>
-  			</div>
-  		</div>
- -->
+  
+    <div class="lop">
+    	<div class="divone">
+			<div class="divtwo">
+				<span class="spanf spone"  @click="houtui()">←</span>
+				<span class="spanf sptwo">服饰配饰专场</span>
+				<span class="spanf spthree">···</span>
+			</div>
+			<div class="divthree">
+				<ul class="">
+					<li class="li-class">人气</li>
+					<li class="li-class" @click="renqi()">折扣</li>
+					<li class="li-class">价格</li>
+					<li class="li-class">筛选</li>
+				</ul>
+			</div>
+		</div>
 
-
-    <div>
-    	<div v-for="products in looplist" @click="hadnleClick(products)">
+	<div v-infinite-scroll="loadMore" infinite-scroll-disabled="busy" infinite-scroll-distance="0">
+    	<div class="lopChild" v-for="products in looplist" @click="hadnleClick(products)">
     		<img :src="products.imageUrl">
-    		<!-- <h3>{{products.productName}}</h3> -->
+    		<h3>{{products.brandName}}</h3>
+    		<h3>{{products.productName}}</h3>
+    		<p class="tan">
+	    		<span class="sp1">￥{{products.itemPriceDto.price}}</span>
+	    		<span class="sp2">￥{{products.marketPrice}}</span>
+	    		<span class="sp3">{{products.itemPriceDto.discount}}折</span>
+    		</p>
     	</div>
-    	<!-- <li>
-    		<img src="https://cdn14.mei.com/product/YEX-113-00094/b87316eec89efc31d8c2875c90d75642.jpg@300w_400h_2e_75q">
-
-    	</li> -->
+	</div>
+    	
     </div>
   </div>
 </template>
@@ -54,70 +37,149 @@
 <script>
 	import Vue from 'vue'
 	import axios from "axios"
+	import infiniteScroll from 'vue-infinite-scroll'
+	Vue.use(infiniteScroll)
+
 	export default {
 		data(){
 			return {
-				looplist:[]
+				looplist:[],
+				busy: false,
+				c:0
 			}
 		},
 		created(){
 
 		},
 
+
 		mounted(){
 			console.log(this.$route.params.myid)
-			axios({
-				url:`http://www.mei.com/appapi/event/product/v3?pageIndex=1&categoryId=2121005100000001991&key=&sort=&timestamp=1550125192415&summary=43259576322a75c7a16d0e7edb6d9b4d&platform_code=H5`,
-
-			}).then(res=>{
-				console.log(res.data.products);
-				this.looplist = res.data.products
-			})
+			
 		},
 		methods:{
-			handleClick(index){
-				this.$router.push(`/productlist/${index}`)
+			loadMore(){
+				this.c++
+				this.busy=true;
+				console.log(233)
+				axios({
+				url:`http://www.mei.com/appapi/event/product/v3?pageIndex=${this.c}&categoryId=2121005100000001991&key=&sort=&timestamp=1550125192415&summary=43259576322a75c7a16d0e7edb6d9b4d&platform_code=H5`
+
+			}).then(res=>{
+				if(res.data.products.length){
+				console.log(res.data.products);
+				this.looplist = [...this.looplist,...res.data.products]
+				this.busy = false;
+			}else{
+				return
 			}
+			})
+			
+			},
+			huoutui(){
+				this.$router.go(-1)
+			},
+			renqi(){
+				axios({
+					url:`http://www.mei.com/appapi/event/product/v3?pageIndex=${this.c}&categoryId=2121005100000001991&key=1&sort=ASC&timestamp=1550231863111&summary=bff679b42a1670469f372cf0e3af7695&platform_code=H5`
+				}).then(res=>{
+					this.looplist = res.data.products
+				})
+			}
+			
+
+			// handleClick(index){
+			// 	this.$router.push(`/productlist/${index}`)
+			// }
 		}
+
 	}
+
 </script>
 
 <style scoped lang="scss">
-	// .topMenuBar[data-v-739eeef1] {
-	//     position: relative;
-// 	    z-index: 13500;
-// 	}
-// 	.topbar[data-v-aa36b710] {
-//     z-index: 9999;
-//     width: 100%;
-//     height: 1.173333rem;
-//     position: relative;
-//     display: -ms-flexbox;
-//     display: -webkit-box;
-//     display: flex;
-//     -ms-flex-direction: row;
-//     -webkit-box-orient: horizontal;
-//     -webkit-box-direction: normal;
-//     flex-direction: row;
-//     -ms-flex-wrap: nowrap;
-//     flex-wrap: nowrap;
-//     -ms-flex-pack: justify;
-//     -webkit-box-pack: justify;
-//     justify-content: space-between;
-//     background-color: hsla(0,0%,100%,.95);
-// }
-	div {
-		 display: flex;
+	
+	.lop {
+		
+		display: flex;
       	flex-direction: row;
       	flex-wrap: wrap;
-      // flex-wrap: no-wrap;
-		// width: 640px;
-	    div{
+      	margin: 20px;
+
+	    .lopChild{
 	    	width: 50%;
+	    	display: inline-block;
 	    	img {
 				width: 100%;
 	    	}
-	    	
+
+	    	h3 {
+	    		// display: block;
+    		    white-space: nowrap;
+			    text-overflow: ellipsis;
+			    overflow: hidden;
+	    	}
 	    }
+	    .tan{
+	    	display: flex;
+	      	flex-direction: row;
+	      	flex-wrap: wrap;
+
+	    	.sp1{
+		    	color: red;
+				width: 30%;
+		    }
+		    .sp2{
+				width: 30%;
+				color: #ccc;
+				text-decoration:line-through
+		    }
+		    .sp3{
+		    	color: #666;
+		    	width: 30%;
+		    }
+	    }
+	    
 	}
+	.divone{
+		width: 100%;
+		height: 1.5rem;
+	}
+	.divtwo{
+		display: flex;
+      	flex-direction: row;
+      	flex-wrap: wrap;
+      	justify-content: space-between;
+      	.spone{
+			font-size: 20px;
+      	}
+      	.sptwo{
+			font-size: 0.575rem;
+      	}
+      	.spthree{
+			font-size: 20px;
+      	}
+      	
+	}
+	.divthree{
+		margin-top: 10px; 
+		display: flex;
+      	flex-direction: row;
+      	flex-wrap: wrap;
+      	ul{
+      		width: 100%;
+      	}
+      	.li-class{
+      		list-style: none;
+      		width: 20%;
+      		float: left;
+      		margin-right: 14px; 
+
+      	}
+      	.li-m{
+      		margin-left: 10px; 
+      	}
+      	
+	}
+	
 </style>
