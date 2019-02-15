f47b745d4608cb0a7b3b60e0741e2e065ce7b509
BigPeiQi <3223364205@qq.com>
2019-02-15 11:38:55 +0800

    哈哈

--- src/views/productlist.vue
+++ src/views/productlist.vue
@@ -1,45 +1,47 @@
 <template>
-  <div class="productlist">
-  	<div data-v-739eeef1="" class="topMenuBar">
-  		<div data-v-aa36b710="" data-v-739eeef1="" class="topbar" id="topBar" style="background: rgb(255, 255, 255);">
-  			<div data-v-aa36b710="" data-spm="NavbarBack" class="left">
-  				<a data-v-aa36b710="" class="back">
-  					<span data-v-aa36b710="" class="iconfont mei-arrow">
+  <!-- <div class="productlist">
+  	<div  class="topMenuBar">
+  		<div  class="topbar" id="topBar" style="background: rgb(255, 255, 255);">
+  			<div class="left">
+  				<a class="back">
+  					<span class="iconfont mei-arrow">
   						
   					</span>
   				</a>
   			</div> 
-  			<div data-v-aa36b710="" class="title">
-  				<span data-v-aa36b710="">服饰配饰专场</span> <!---->
+  			<div class="title">
+  				<span >服饰配饰专场</span> 
   			</div> 
-  			<div data-v-aa36b710="" class="right">
-  				<div data-v-aa36b710="" data-spm="NavbarSwitch" class="switch-menu">
-  					<a data-v-aa36b710="" class="show-menu">
-  						<span data-v-aa36b710="" class="iconfont mei-dot">
+  			<div class="right">
+  				<div class="switch-menu">
+  					<a class="show-menu">
+  						<span class="iconfont mei-dot">
   							
   						</span>
-  					</a> <!---->
+  					</a> 
   				</div> 
-  				<!----></div>
+  				</div>
   			</div> 
-  			<div data-v-7a0672c3="" data-v-739eeef1="" class="menu">
-  				<div data-v-7a0672c3="" class="box">
-  					<ul data-v-7a0672c3="">
-  						<li data-v-7a0672c3="" class="current">人气</li> 
-  						<li data-v-7a0672c3="" class="">折扣</li> 
-  						<li data-v-7a0672c3="" class="price up">
-  							<span data-v-7a0672c3="">价格</span>
+  			<div class="menu">
+  				<div  class="box">
+  					<ul>
+  						<li class="current">人气</li> 
+  						<li class="">折扣</li> 
+  						<li class="price up">
+  							<span>价格</span>
   						</li> 
-  						<li data-v-7a0672c3="" class="filter">筛选</li>
+  						<li class="filter">筛选</li>
   					</ul>
   				</div>
   			</div>
   		</div>
+ -->
+
 
     <div>
     	<div v-for="products in looplist" @click="hadnleClick(products)">
     		<img :src="products.imageUrl">
-    		<h3>{{products.productName}}</h3>
+    		<!-- <h3>{{products.productName}}</h3> -->
     	</div>
     	<!-- <li>
     		<img src="https://cdn14.mei.com/product/YEX-113-00094/b87316eec89efc31d8c2875c90d75642.jpg@300w_400h_2e_75q">
@@ -81,33 +83,41 @@
 </script>
 
 <style scoped lang="scss">
-	.topMenuBar[data-v-739eeef1] {
-	    position: relative;
-	    z-index: 13500;
+	// .topMenuBar[data-v-739eeef1] {
+	//     position: relative;
+// 	    z-index: 13500;
+// 	}
+// 	.topbar[data-v-aa36b710] {
+//     z-index: 9999;
+//     width: 100%;
+//     height: 1.173333rem;
+//     position: relative;
+//     display: -ms-flexbox;
+//     display: -webkit-box;
+//     display: flex;
+//     -ms-flex-direction: row;
+//     -webkit-box-orient: horizontal;
+//     -webkit-box-direction: normal;
+//     flex-direction: row;
+//     -ms-flex-wrap: nowrap;
+//     flex-wrap: nowrap;
+//     -ms-flex-pack: justify;
+//     -webkit-box-pack: justify;
+//     justify-content: space-between;
+//     background-color: hsla(0,0%,100%,.95);
+// }
+	div {
+		 display: flex;
+      	flex-direction: row;
+      	flex-wrap: wrap;
+      // flex-wrap: no-wrap;
+		// width: 640px;
+	    div{
+	    	width: 50%;
+	    	img {
+				width: 100%;
+	    	}
+	    	
+	    }
 	}
-	.topbar[data-v-aa36b710] {
-    z-index: 9999;
-    width: 100%;
-    height: 1.173333rem;
-    position: relative;
-    display: -ms-flexbox;
-    display: -webkit-box;
-    display: flex;
-    -ms-flex-direction: row;
-    -webkit-box-orient: horizontal;
-    -webkit-box-direction: normal;
-    flex-direction: row;
-    -ms-flex-wrap: nowrap;
-    flex-wrap: nowrap;
-    -ms-flex-pack: justify;
-    -webkit-box-pack: justify;
-    justify-content: space-between;
-    background-color: hsla(0,0%,100%,.95);
-}
-	// div {
-	//     width: 4.4rem;
-	//     height: 8.053333rem;
-	//     margin-bottom: .266667rem;
-	//     display: inline-block;
-	// }
 </style>
